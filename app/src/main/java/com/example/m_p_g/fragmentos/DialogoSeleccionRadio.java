9716f12aa99be7c5f3023bd62a632e4def8cb00e
MelchorPech <15390232@itchetumal.edu.mx>
2018-12-07 20:22:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/m_p_g/fragmentos/DialogoSeleccionRadio.java
@@ -0,0 +1,29 @@
+package com.example.m_p_g.fragmentos;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+
+public class DialogoSeleccionRadio extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final String[] items = {"Español", "Inglés", "Francés"};
+
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Selección Radio")
+         .setSingleChoiceItems(items, -1,
+        		   new DialogInterface.OnClickListener() {
+	    	    public void onClick(DialogInterface dialog, int item) {
+	    	        Log.i("Dialogos", "Opción elegida: " + items[item]);
+	    	    }
+	    	});
+        return builder.create();
+    }
+}
